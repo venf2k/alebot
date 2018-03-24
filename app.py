a7c0028bedeeba0888793437b41f6119ec76948f
venf2k <venf2k@yahoo.it>
2018-03-24 17:30:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def verify_fb_token(token_sent):
 
 #chooses a random message to send to the user
 def get_message():
-    sample_responses = ["You are stunning!", "Ciao, come posso aiutarti?", "Bello poter parlare con te!", "Ti ringrazio. :)"]
+    sample_responses = ["E' stato bello conoscerti!", "Ciao, come posso aiutarti?", "Bello poter parlare con te!", "Ti ringrazio. :)"]
     # return selected item to the user
     return random.choice(sample_responses)
 
